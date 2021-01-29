e7f54295cc65df85b67efbaf6df83be4e2b2d9d8
Jakson Jose <jaksonjosesilva34@gmail.com>
2021-01-29 19:19:17 -0300

    Bug fixed and added .gitignore

--- src/script.js
+++ src/script.js
@@ -14,7 +14,6 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing, playDice, player;
-player = localStorage.getItem('player-0');
 
 const initilization = () => {
     // Starting condition
@@ -41,28 +40,29 @@ initilization();
 
 
 const SwitchPlayer = () => {
-    console.log(activePlayer)
+
     document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     playerA.classList.toggle('player--active');
     playerB.classList.toggle('player--active');
     console.log(activePlayer)
-
-    player = localStorage.getItem(`player-${activePlayer}`);
+    
 }
 
 
 //When player click on Roll Dice button
 const DiceRoll = () => {
     if (playing) {
+        
         // 1 - Generating a random dice roll
         playDice = Math.trunc(Math.random() * 6 ) + 1;       
 
         // 2 - Display dice
         diceImg.classList.remove('hidden');
         diceImg.src = `image/dice-${playDice}.png`;
+        
+        player = localStorage.getItem(`player-${activePlayer}`);
 
         // Check for rolled is not 1 and sum up to current score
         if (playDice !== 1) {
@@ -82,7 +82,7 @@ const Hold = () => {
    if (playing) {
         scores[activePlayer] += currentScore;
         document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
-        
+        player = localStorage.getItem(`player-${activePlayer}`);
 
         // 2 - check if players's score is >= 100
         if (scores[activePlayer] >= 100){
@@ -106,15 +106,16 @@ btnHold.addEventListener('click', () => Hold())
 btnNew.addEventListener('click', () => initilization());
 
 
+
 /** ***Modal alert (When hold, when got one Pipe*** **/
 const modalDisplay = document.querySelector('.modal-alert');
 const modalTitle = document.querySelector('.modal-title');
 const modalText = document.querySelector('.modal-text');
 const modalImg = document.querySelector('.modal-img');
-
+const btnPlay = document.querySelector('.btn-next');
 const namePlayer = document.querySelector('#name--01');
 
-const btnPlay = document.querySelector('.btn-next');
+
 
 const DiceRolledOne = () => {
     modalDisplay.classList.remove('modal-alert-hidden');
@@ -125,8 +126,8 @@ const DiceRolledOne = () => {
 
 const PlayerHitHold = () => {
     modalDisplay.classList.remove('modal-alert-hidden');
-    modalTitle.innerHTML = `Dude! <p> You scored ${currentScore}.</p>`;
-    modalText.innerHTML = `${player}, now it's your turn. Good Luck!`
+    modalTitle.innerHTML = `Dude! <p>You scored ${currentScore}.</p>`;
+    modalText.innerHTML = `Now is ${player} turn. Good Luck!`
     modalImg.src = 'image/dice-one-side.png';
 
 }
